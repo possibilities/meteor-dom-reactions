6cfaf62c708541170f9f4e576229d7271d124a28
Mike Bannister <notimpossiblemike@gmail.com>
2012-06-23 20:16:05 -0400

    get rid of colors

--- src/dom-reactions/client.js
+++ src/dom-reactions/client.js
@@ -4,26 +4,6 @@ _.mixin(_.str.exports());
 
   Reactions = new Meteor.Collection(null);
 
-  var colorIndex = 0;
-  var colors = [
-    'green',
-    'red',
-    'blue',
-    'orange',
-    'purple',
-    'fuchsia',
-    'indigo',
-    'gray'
-  ];
-
-  var nextColor = function() {
-    if (colorIndex >= colors.length)
-      colorIndex = 0;
-    var color = colors[colorIndex];
-    colorIndex++;
-    return color;
-  };
-
   var previousStamp;
   Handlebars.registerHelper('stamp', function(reaction) {
     if (this.stamp !== previousStamp) {
